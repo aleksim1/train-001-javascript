1d2ede6f67557f6e049010688bfc2b4fa39330b0
aleksim1 <aleksi.moilanen02@gmail.com>
2024-09-18 12:00:50 +0300

    workshop4

--- Workshop4/4.1_Task_1_Multiplication_Tables_2.js
+++ Workshop4/4.1_Task_1_Multiplication_Tables_2.js
@@ -1,9 +1,14 @@
 /**Print out multiplication tables for numbers 1-10 using a loop. Use nested for
 loops. */
 for (let i=1;i<10;i++){
-    process.stdout.write("***************************\n")
-    process.stdout.write("Multiplication table for "+i+"\n")
+    
+    process.stdout.write("Multiplication table for "+i+"\n");
+    process.stdout.write("*".repeat(26)+"\n");
     for (let y = 1;y <= 10; y++){
-        process.stdout.write(`*${i.toString().padStart(11)}*${y}=${(i*y).toString().padEnd(11)}*\n`)
+        let table =(`${i.toString()}*${y}=${(i*y).toString()}`);
+        process.stdout.write("*"+table.padStart(15)+"*".padStart(10)+"\n");
+        
     }
-}   
+    process.stdout.write("*".repeat(26)+"\n");
+    
+}   process.exit()
