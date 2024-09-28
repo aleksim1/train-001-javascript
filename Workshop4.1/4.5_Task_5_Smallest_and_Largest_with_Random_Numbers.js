1c1e735fc8e7014f650951889d3dff09a73ef77d
aleksim1 <aleksi.moilanen02@gmail.com>
2024-09-28 23:18:28 +0300

    4.5

--- Workshop4.1/4.5_Task_5_Smallest_and_Largest_with_Random_Numbers.js
+++ Workshop4.1/4.5_Task_5_Smallest_and_Largest_with_Random_Numbers.js
@@ -1,6 +1,19 @@
-const randomArray = Array(100).fill().map(() => Math.round(Math.random() * 1000))
-console.table(randomArray)
-
-for (let i = 0; i < randomArray.length; i++){
-    
+const randomArray = Array(100).fill().map(() => Math.round(Math.random() * 1000));
+function findLargestNumber (array){
+    let largest = array[0];
+for (let i = 0; i < array.length; i++){
+    if (largest<array[i]){
+    largest = array[i];
+    } 
+} process.stdout.write(`${largest} Is the largest number.\n`);
 }
+function findSmallestNumber (array){
+    let smallest = array[0];
+for (let i = 1; i < array.length; i++){
+    if (smallest>array[i]){
+    smallest = array[i];
+    } 
+} process.stdout.write(`${smallest} Is the smallest number.\n`);
+}
+findLargestNumber(randomArray);
+findSmallestNumber(randomArray);
